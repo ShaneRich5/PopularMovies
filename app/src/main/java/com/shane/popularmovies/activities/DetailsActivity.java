68fa40e2e199729e67bd5f91cea1f7a55e56ba5e
Shane Richards <shane.richards121@gmail.com>
2016-08-31 11:32:55 -0500

    refactored DetailsActivity

--- app/src/main/java/com/shane/popularmovies/activities/DetailsActivity.java
+++ app/src/main/java/com/shane/popularmovies/activities/DetailsActivity.java
@@ -2,11 +2,13 @@ package com.shane.popularmovies.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 
 import com.google.gson.Gson;
 import com.shane.popularmovies.R;
@@ -17,6 +19,7 @@ import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -29,24 +32,40 @@ import rx.schedulers.Schedulers;
 public class DetailsActivity extends AppCompatActivity {
     public static final String TAG = DetailsActivity.class.getName();
 
+    @BindView(R.id.coordinator_container) CoordinatorLayout containerCoordinatorLayout;
     @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.fab_favourite) FloatingActionButton favouriteFab;
 
     private OkHttpClient client = new OkHttpClient();
+
+    private int lolCounter = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         ButterKnife.bind(this);
-
         setSupportActionBar(toolbar);
 
         Log.i(TAG, "id=" + getMovieIdFromIntent());
 //        loadMovieData();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
+    }
+
+    @OnClick(R.id.fab_favourite)
+    public void onFavouriteFabClicked(View v){
+        String message;
+
+        if (lolCounter <= 1)
+            message = "Didnt finish yet :(";
+        else if (lolCounter == 2)
+            message = "Just wait nuh :/";
+        else if (lolCounter == 3)
+            message = "The sumn nah work -.-";
+        else
+            message = "Sigh...";
+
+        Snackbar.make(containerCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
     }
 
     private void loadMovieData() {
