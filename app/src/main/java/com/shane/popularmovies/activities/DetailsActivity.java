b4c26bc0194f1780b7a11b8a477d65d8ab7aded9
Shane Richards <shane.richards121@gmail.com>
2016-09-04 16:56:03 -0500

    removed fab

--- app/src/main/java/com/shane/popularmovies/activities/DetailsActivity.java
+++ app/src/main/java/com/shane/popularmovies/activities/DetailsActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
